77e7015673a45c72632034caf23eeabf51c7975d
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2011-03-10 13:09:27 +0100

    frquotes.hs: force reading UTF-8

--- frquotes.hs
+++ frquotes.hs
@@ -1,15 +1,31 @@
 import System.Environment
+import System.IO
 import Text.FrQuotes (frQuotes)
 
-interact' :: (String -> String) -> IO ()
-interact' f = do
+readFileEnc :: TextEncoding -> FilePath -> IO String
+readFileEnc enc fp = do
+  fh <- openFile fp ReadMode
+  hSetEncoding fh enc
+  hGetContents fh
+
+writeFileEnc :: TextEncoding -> FilePath -> String -> IO ()
+writeFileEnc enc fp s =
+  withFile fp WriteMode $ \fh ->
+    hSetEncoding fh enc >>
+    hPutStr fh s
+
+interact' :: TextEncoding -> (String -> String) -> IO ()
+interact' enc f = do
   args <- getArgs
   case args of
-    [] -> interact f
+    [] -> do
+      hSetEncoding stdin  enc
+      hSetEncoding stdout enc
+      interact f
     [in1,inp,outp] ->
-      writeFile outp . g . f =<< readFile inp
+      writeFileEnc enc outp . g . f =<< readFileEnc enc inp
       where g = (("{-# LINE 1 \""++in1++"\" #-}\n")++)
     _  -> fail "Usage: frquotes [<orig> <input> <output>]"
 
 main :: IO ()
-main = interact' frQuotes
+main = interact' utf8 frQuotes
